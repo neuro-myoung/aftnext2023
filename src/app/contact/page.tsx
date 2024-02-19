66a943ad7ac9b3ceba0015d7880559b00262d85b
michael young <neuromyoung@gmail.com>
2024-02-18 20:36:53 -0500

    removed apostrophe

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import styles from './contact.module.css'
-import ContactForm from './ContactForm'
+import React from 'react';
+import styles from './contact.module.css';
+import ContactForm from './ContactForm';
 
 const Contact = () => {
   return (
     <div className={styles.contactWrapper}>
-      <h1>Let's Connect</h1>
+      <h1>Lets Connect</h1>
       <div className={styles.formWrapper}>
         <div className={styles.descriptionWrapper}>
           <p>Have questions, want to connect, or commission custom artwork? Send me a message or reach out on social media.</p>
