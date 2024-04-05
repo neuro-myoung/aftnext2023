c9f55c52f2d7e5fb6c588ff8469d1bd83fa67fdd
Michael Young <neuromyoung@gmail.com>
2024-04-04 21:28:33 -0400

    minor changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './contact.module.css';
 import ContactForm from './ContactForm';
+import { SocialIcon } from 'react-social-icons';
+import { TfiEmail } from "react-icons/tfi";
+import { FaFacebook, FaInstagram, FaTiktok, FaYoutube} from "react-icons/fa";
+import Link from 'next/link'
 
 const Contact = () => {
   return (
@@ -9,6 +13,20 @@ const Contact = () => {
       <div className={styles.formWrapper}>
         <div className={styles.descriptionWrapper}>
           <p>Have questions, want to connect, or commission custom artwork? Send me a message or reach out on social media.</p>
+            <div className={styles.contactMethods}>
+              <h2>Follow me on Social Media to see the latest</h2>
+              <div className={styles.socialIcons}>
+                <Link href="https://www.instagram.com/alixfuerst_art/" ><FaInstagram className={styles.icon}/></Link>
+                <Link href="https://www.tiktok.com/@alixfuerst_art" ><FaTiktok className={styles.icon}/></Link>
+                <Link href="https://www.facebook.com/AlixFuerstArt/" ><FaFacebook className={styles.icon}/></Link>
+                <Link href="https://www.youtube.com/c/alixfuerstart" ><FaYoutube className={styles.icon} /></Link>
+              </div>
+              <div className={styles.emailWrapper}> 
+              <Link href="mailto:alix.fuerst@gmail.com" ><TfiEmail className={styles.icon} /></Link>
+              <p>alix.fuerst@gmail.com</p>
+              </div>
+              <p> Based in Medford, MA and locally serving the greater Boston area</p>
+            </div>
         </div>
         <ContactForm />
       </div>
